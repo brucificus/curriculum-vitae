96ff9d7dad64abbef608c28cde12d5112e37243e
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-01-02 15:25:10 -0500

    ♻ Nests Props Members
    This allows us to introduce other props members at a higher conceptual level.

--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -4,23 +4,29 @@ import { IProject, projectPropTypes } from "../models/project";
 import jobItemStyles from "./project-item.module.scss";
 import TimeRange from "./time-range";
 
-const JobItem = (props: IProject) =>
+interface IProjectItemProps {
+    project: IProject
+};
+
+const ProjectItem = (props: IProjectItemProps) =>
     <article className={jobItemStyles.container}>
         <header>
-            <span className={jobItemStyles.roleFinal}>{props.role_final}</span>
-            <span className={jobItemStyles.projectName}>{props.project_name}</span>
-            <span className={jobItemStyles.projectLocation}>{props.project_location}</span>
-            <span className={jobItemStyles.projectTimeRange}><TimeRange start={props.project_start} end={props.project_end}/></span>
+            <span className={jobItemStyles.roleFinal}>{props.project.role_final}</span>
+            <span className={jobItemStyles.projectName}>{props.project.project_name}</span>
+            <span className={jobItemStyles.projectLocation}>{props.project.project_location}</span>
+            <span className={jobItemStyles.projectTimeRange}><TimeRange start={props.project.project_start} end={props.project.project_end}/></span>
         </header>
         <main>
             <ul>
-                {props.accomplishments.map(i =>
+                {props.project.accomplishments.map(i =>
                     <li dangerouslySetInnerHTML={{ __html: i }} />
                 )}
             </ul>
         </main>
     </article>;
 
-export default JobItem;
+export default ProjectItem;
 
-JobItem.propTypes = projectPropTypes();
+ProjectItem.propTypes = {
+    project: projectPropTypes()
+};
