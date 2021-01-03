f6c396ac02aed2afd4ab8baaeffca08cf7a3939f
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-01-03 17:55:04 -0500

    ♻ Expands Profile to Support Dynamic Selection by Resume Variant

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -7,44 +7,58 @@
 import * as path from "path";
 import { Actions } from "gatsby";
 
+import { IProfile, ProfilePredicates } from "./src/models/profile";
+
 type GraphqlDirectQuery = <TResult>(query: TemplateStringsArray | string) => Promise<TResult>;
-interface IMarkdownRemarkQueryResult {
-    data: {
-        allMarkdownRemark: {
-            edges: [
-                {
-                    node: {
-                        html: string
+
+async function getAllResumeProfileVariants(graphql: GraphqlDirectQuery): Promise<IProfile[]> {
+    interface IProfileVariantsYamlQueryResult {
+        data: {
+            allProfileVariantsYaml: {
+                edges: [
+                    {
+                        node: IProfile
                     }
-                }
-            ]
+                ]
+            }
         }
     }
+
+    const allResumeProfileVariantsQueryResult = await graphql<IProfileVariantsYamlQueryResult>(`
+    query {
+        allProfileVariantsYaml(filter: {}) {
+          edges {
+            node {
+              content
+              for_resume_variants
+            }
+          }
+        }
+      }
+    `);
+    
+    return allResumeProfileVariantsQueryResult.data.allProfileVariantsYaml.edges.map(e => e.node);
 }
 
 export async function createPages({ graphql, actions }: { graphql: GraphqlDirectQuery, actions: Actions }): Promise<void> {
     const { createPage } = actions;
     const displayResumeTemplate = path.resolve('src/templates/display-resume.tsx');
 
-    const profileContentHtmlQueryResult = await graphql<IMarkdownRemarkQueryResult>(`
-    query {
-        allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(attributes)\\/(profile)/"}}) {
-          edges {
-            node {
-              html
-            }
-          }
-        }
-    }`);
-    const profileContentHtml = profileContentHtmlQueryResult.data.allMarkdownRemark.edges[0].node.html;
+    const allResumeProfileVariants = await getAllResumeProfileVariants(graphql);
+    const getResumeProfileVariantOrDefault = function (resumeVariant: string): IProfile {
+        const specificIfFound = allResumeProfileVariants.filter(ProfilePredicates.forSpecificResumeVariant(resumeVariant))[0];
+        const defaultIfFound = allResumeProfileVariants.filter(ProfilePredicates.forDefault())[0];
+        return specificIfFound || defaultIfFound;
+    }
 
+    const resumeVariant = "srsweng";
     createPage({
         path: '/',
         component: displayResumeTemplate,
         context: {
-            resumeVariant: "srsweng",
+            resumeVariant: resumeVariant,
             professionText: "Senior Software Engineer",
-            profileContentHtml: profileContentHtml
+            profileContentHtml: getResumeProfileVariantOrDefault(resumeVariant).content
         }
     });
 };
