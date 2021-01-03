231e699d7e4f314c30f3af2293b49a41bcda9c02
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-01-03 14:08:16 -0500

    ♻ Parameterizes Profile Content

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -7,16 +7,44 @@
 import * as path from "path";
 import { Actions } from "gatsby";
 
-export function createPages({ graphql, actions }: { graphql: any, actions: Actions }) {
+type GraphqlDirectQuery = <TResult>(query: TemplateStringsArray | string) => Promise<TResult>;
+interface IMarkdownRemarkQueryResult {
+    data: {
+        allMarkdownRemark: {
+            edges: [
+                {
+                    node: {
+                        html: string
+                    }
+                }
+            ]
+        }
+    }
+}
+
+export async function createPages({ graphql, actions }: { graphql: GraphqlDirectQuery, actions: Actions }): Promise<void> {
     const { createPage } = actions;
     const displayResumeTemplate = path.resolve('src/templates/display-resume.tsx');
+
+    const profileContentHtmlQueryResult = await graphql<IMarkdownRemarkQueryResult>(`
+    query {
+        allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(attributes)\\/(profile)/"}}) {
+          edges {
+            node {
+              html
+            }
+          }
+        }
+    }`);
+    const profileContentHtml = profileContentHtmlQueryResult.data.allMarkdownRemark.edges[0].node.html;
 
     createPage({
         path: '/',
         component: displayResumeTemplate,
         context: {
             resumeVariant: "srsweng",
-            professionText: "Senior Software Engineer"
+            professionText: "Senior Software Engineer",
+            profileContentHtml: profileContentHtml
         }
     });
 };
