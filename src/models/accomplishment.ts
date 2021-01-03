94f3d516b1d7bda8cbc6f160b4e905b69977428a
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-01-02 22:34:48 -0500

    🏗 Parameterizes Filtration of Job and Project Accomplishments
    This completes initial support for the filtration of each job's/project's accomplishments at the page level.

--- src/models/accomplishment.ts
+++ src/models/accomplishment.ts
@@ -11,3 +11,36 @@ export function accomplishmentPropType(): PropTypes.Validator<PropTypes.InferTyp
         for_resume_variants: PropTypes.string.isRequired
     });
 };
+
+export type AccomplishmentPredicate = (value: IAccomplishment) => boolean;
+
+export class AccomplishmentPredicates {
+    public static forAll(): AccomplishmentPredicate {
+        const parseResumeVariantsSpecifier = (value: string): string[] => value.split(",").map(j => j.trim().toLowerCase());
+
+        return function _forAll(i) {
+            const parsedResumeVariantsSpecifier = parseResumeVariantsSpecifier(i.for_resume_variants);
+            return parsedResumeVariantsSpecifier.includes("all");
+        };
+    }
+
+    public static forAllOrSpecificResumeVariant(resume_variant: string): AccomplishmentPredicate {
+        const parseResumeVariantsSpecifier = (value: string): string[] => value.split(",").map(j => j.trim().toLowerCase());
+
+        return function _forAllOrSpecificResumeVariant(i) {
+            const parsedResumeVariantsSpecifier = parseResumeVariantsSpecifier(i.for_resume_variants);
+            return parsedResumeVariantsSpecifier.includes("all") || parsedResumeVariantsSpecifier.includes(resume_variant.toLowerCase());
+        };
+    }
+
+    public static forSpecificResumeVariant(resume_variant: string): AccomplishmentPredicate {
+        const parseResumeVariantsSpecifier = (value: string): string[] => value.split(",").map(j => j.trim().toLowerCase());
+
+        return function _forSpecificResumeVariant(i) {
+            const parsedResumeVariantsSpecifier = parseResumeVariantsSpecifier(i.for_resume_variants);
+            return parsedResumeVariantsSpecifier.includes(resume_variant.toLowerCase());
+        };
+    }
+};
+
+export function accomplishmentPredicatePropType() { return PropTypes.func; }
