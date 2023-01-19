d30a09f9c90666dddc09586f63bb7c730f64a2b0
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-01-19 07:06:04 -0500

    💄 Layout Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ require('ts-node').register();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bruce Markham's Resume`,
+    description: `The resume of Bruce Markham.`,
+    author: `Bruce Markham`,
   },
   pathPrefix: "/curriculum-vitae",
   plugins: [
